bd029081bf63160e6da4f231ea28ae506950fdc8
Dustin Egos <140057465+Dustin0829@users.noreply.github.com>
2025-08-24 18:54:16 +0800

    fixed sidebar

--- app/files/page.tsx
+++ app/files/page.tsx
@@ -1,5 +1,7 @@
 "use client"
 
+import { Sidebar } from "@/components/layout/sidebar"
+import { Header } from "@/components/layout/header"
 import { LazyFileList } from "@/components/lazy-components"
 
 // Disable SSR for this page
@@ -7,5 +9,15 @@ export const dynamic = 'force-dynamic'
 export const ssr = false
 
 export default function FilesPage() {
-  return <LazyFileList />
+  return (
+    <div className="flex min-h-screen w-full bg-background">
+      <Sidebar />
+      <div className="flex flex-1 flex-col overflow-y-auto">
+        <Header />
+        <main className="flex flex-1 flex-col gap-4 p-4 md:gap-8 md:p-8">
+          <LazyFileList />
+        </main>
+      </div>
+    </div>
+  )
 }
